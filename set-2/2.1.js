206ca72057b54be73afde747575580492afe1941
Andrey <andrydmit@gmail.com>
2019-02-01 23:41:21 +0200

    esercise 2.1 complete v1

--- set-2/2.1.js
+++ set-2/2.1.js
@@ -1,28 +1,32 @@
-function encrypt(str, offset) {
-  let arr = str.split("");
-  arr = arr.map(k => shiftValueInRangePlus(k, offset));
+function encrypt(str, offset = 0) {
+  if (offset !== 0) {
+    let arr = str.split("");
+    arr = arr.map(k => shiftValueInRangePlus(k, offset));
 
-  str = arr.join('');
+    str = arr.join("");
 
-  return str;
+    return str;
+  } else {
+    return str;
+  }
 }
 
-function decrypt(str, offset) {
-  let arr = str.split("");
-  arr = arr.map(k => shiftValueInRangeMinus(k, offset));
+function decrypt(str, offset = 0) {
+  if (offset !== 0) {
+    let arr = str.split("");
+    arr = arr.map(k => shiftValueInRangeMinus(k, offset));
 
-  str = arr.join('');
+    str = arr.join("");
 
-  return str;
+    return str;
+  } else {
+    return str;
+  }
 }
 
-console.log(encrypt("abz", 3));
-console.log(decrypt("dec", 3)); // "abz"
-// console.log(decrypt(encrypt("foobar")) == "foobar");
-
-// encrypt("abz", 3); // "dec"
-// decrypt("dec", 3); // "abz"
-// decrypt(encrypt("foobar")) == "foobar"; // true
+console.log(encrypt("abz", 0)); // "dec"
+console.log(decrypt("dec", 0)); // "abz"
+console.log(decrypt(encrypt("foobar")) == "foobar"); // true
 
 function shiftValueInRangePlus(value, offset) {
   value = value.charCodeAt();
@@ -30,10 +34,10 @@ function shiftValueInRangePlus(value, offset) {
   const from = 97;
 
   if (value >= from && value <= to) {
-    let shiftedValue = value + offset > to ? (value + offset - to + from - 1) : (value + offset);
+    let shiftedValue =
+      value + offset > to ? value + offset - to + from - 1 : value + offset;
     shiftedValue = String.fromCharCode(shiftedValue);
     return shiftedValue;
-
   } else return "value not in range";
 }
 
@@ -43,9 +47,22 @@ function shiftValueInRangeMinus(value, offset) {
   const from = 97;
 
   if (value >= from && value <= to) {
-    value = value - offset < from ? (value - offset - from  + to + 1) : (value - offset);
+    value =
+      value - offset < from ? value - offset - from + to + 1 : value - offset;
     value = String.fromCharCode(value);
     return value;
-
   } else return "value not in range";
 }
+
+// function shiftValueInRange(value, offset) {
+//   value = value.charCodeAt();
+//   const to = 122;
+//   const from = 97;
+
+//   if (value >= from && value <= to) {
+//     let shiftedValue =
+//       value + offset > to ? value + offset - to + from - 1 : value + offset;
+//     shiftedValue = String.fromCharCode(shiftedValue);
+//     return shiftedValue;
+//   } else return "value not in range";
+// }
