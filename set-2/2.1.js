bd475cfefb04e84e84fe5c6dfa1f2429b98e5544
frayz <andrydmit@gmail.com>
2019-02-01 15:53:46 +0200

    2.1 exercise doing

--- set-2/2.1.js
+++ set-2/2.1.js
@@ -1,28 +1,51 @@
 function encrypt(str, offset) {
   let arr = str.split("");
-  arr.map(k => shiftValueInRange(k, offset));
+  arr = arr.map(k => shiftValueInRangePlus(k, offset));
 
-  return arr;
+  str = arr.join('');
+
+  return str;
 }
 
-function decrypt(str, offset) {}
+function decrypt(str, offset) {
+  let arr = str.split("");
+  arr = arr.map(k => shiftValueInRangeMinus(k, offset));
 
-console.log(encrypt("abc", 4));
+  str = arr.join('');
+
+  return str;
+}
+
+console.log(encrypt("abz", 3));
+console.log(decrypt("dec", 3)); // "abz"
+// console.log(decrypt(encrypt("foobar")) == "foobar");
 
 // encrypt("abz", 3); // "dec"
 // decrypt("dec", 3); // "abz"
 // decrypt(encrypt("foobar")) == "foobar"; // true
 
-// [97 - 122]
-
-function shiftValueInRange(value, offset) {
+function shiftValueInRangePlus(value, offset) {
   value = value.charCodeAt();
   const to = 122;
   const from = 97;
 
-  console.log(encrypt(value, offset));
+  if (value >= from && value <= to) {
+    let shiftedValue = value + offset > to ? (value + offset - to + from - 1) : (value + offset);
+    shiftedValue = String.fromCharCode(shiftedValue);
+    return shiftedValue;
+
+  } else return "value not in range";
+}
+
+function shiftValueInRangeMinus(value, offset) {
+  value = value.charCodeAt();
+  const to = 122;
+  const from = 97;
 
   if (value >= from && value <= to) {
-    return value + offset > to ? value + offset - to + from : value + offset;
+    value = value - offset < from ? (value - offset - from  + to + 1) : (value - offset);
+    value = String.fromCharCode(value);
+    return value;
+
   } else return "value not in range";
 }
