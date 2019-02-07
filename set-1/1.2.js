6a9f9e1d288cd55e049577fe7a95d788ecef718d
Andrey <andrydmit@gmail.com>
2019-02-08 00:03:32 +0200

    exercise 3.4 is done

--- set-1/1.2.js
+++ set-1/1.2.js
@@ -1,51 +1,25 @@
 function deepMerge(o1, o2) {
-  let mergeObj = { ...o1 };
-  const notUsedVal = 1;
+  let obj = { o1 };
 
-  const reducer = (_, currentValue) => {
-    if (isObject(o2[currentValue])) {
-      return (mergeObj[currentValue] = deepMerge(
-        o1[currentValue],
-        o2[currentValue]
-      ));
+  obj = Object.keys(o2).reduce((acc, c) => {
+    if (isObject(o2[c])) {
+      return deepMerge(obj[c], o2[c]);
+      console.log("hekku");
     } else {
-      return (mergeObj = merge(mergeObj, { [currentValue]: o2[currentValue] }));
+      return (acc = o2[c]);
+      console.log("hekkghfgju");
     }
-  };
+  }, {});
 
-  // let obj = { ...o1 };
-
-  // let objCopy = Object.keys(obj).reduce((acc, curr) => {
-  //   {
-  //     acc[curr] = o2[curr];
-  //   }
-  //   return acc;
-  // }, {});
-
-  // console.log(objCopy);
-
-  return objCopy;
+  return obj;
 }
 
-let o1 = { x: { y: "x.y" } };
-let o2 = { x: { z: "x.z" } };
+let o1 = { x: { y: "x.y" }, y: 5 };
+let o2 = { x: { z: "x.z" }, y: 6 };
 
 console.log(deepMerge(o1, o2)); // {x: {y: "x.y", z: "x.z"}}
 // console.log(deepMerge(o2, o1)); // {x: {z: "x.z", y: "x.y"}}
 
-function merge(o1, o2) {
-  return Object.assign({}, o1, o2);
-}
-
 function isObject(item) {
   return typeof item === "object" && item !== null;
 }
-
-/* 
-const reducer = (acc, currentValue) => {
-  return acc + currentValue;
-}
-
-const total = [1, 2, 3, 4, 5].reduce(reducer);
-
-*/
