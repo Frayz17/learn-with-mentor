8166d9390bb570b8c444055741e4ab7e5d9217f2
Andrey <andrydmit@gmail.com>
2019-02-08 09:20:30 +0200

    exercise 3.4 is done

--- set-3/3.4.js
+++ set-3/3.4.js
@@ -1,18 +1,35 @@
 // Task 3.4
-// Given a string of digits, output all the contiguous substrings of 
+// Given a string of digits, output all the contiguous substrings of
 // length n in that string in the order that they appear.
 
-// For example, the string "49142" has the following 3-digit series: 
+// For example, the string "49142" has the following 3-digit series:
 // "491", "914", "142"
 // And the following 4-digit series: "4914", "9142".
 
-// And if you ask for a 6-digit series from a 5-digit string, 
+// And if you ask for a 6-digit series from a 5-digit string,
 // you deserve whatever you get.
-// Note that these series are only required to occupy adjacent positions in the input; 
+// Note that these series are only required to occupy adjacent positions in the input;
 // the digits need not be numerically consecutive.
 
-str = '49142';
+str = "49142";
 
 function substrings(str) {
-//   str.slice(0 + 1, n) while (start + n) < str.length
+  let digits = str.split("").map(s => parseInt(s));
+
+  let slices = n => {
+    let sequence = digits.reduce((acc, _, i, a) => {
+      if (a.slice(i, i + n).length === n) {
+        acc.push(a.slice(i, i + n));
+      }
+      return acc;
+    }, []);
+
+    let str = sequence.map(x => x.join(""));
+    return str;
+  };
+
+  return { slices };
 }
+
+console.log(substrings(str).slices(2)); // [ '49', '91', '14', '42' ]
+console.log(substrings(str).slices(3)); // [ '491', '914', '142' ]
