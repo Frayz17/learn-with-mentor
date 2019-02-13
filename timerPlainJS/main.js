7b818c589c9230a893d4172195591b76b82a29d4
Andrey <andrydmit@gmail.com>
2019-02-14 01:21:18 +0200

    doing timer project

--- timerPlainJS/main.js
+++ timerPlainJS/main.js
@@ -9,11 +9,15 @@ class EventEmitter {
     }
 
     this.events[eventName].push(fn);
+
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+    }
   }
 
   emit(eventName, data) {
     const event = this.events[eventName];
-    if(event) {
+    if (event) {
       event.forEach(fn => {
         fn.call(null, data);
       });
@@ -22,22 +26,113 @@ class EventEmitter {
 }
 
 
-const timer = document.querySelector('.timer');
-const btnStart = document.querySelector('.btn-start');
-const btnStop = document.querySelector('.btn-stop');
-const btnRestart = document.querySelector('.btn-restart');
-let timerValue = parseInt(timer.firstChild.nodeValue);
-
-
-
-
-btnStart.addEventListener('click', () => {
-  emitter.emit('event:timerStart', {timerValue: timerValue});
-});
-
+const timerDir = document.querySelector("#timer");
+const btnStart = document.querySelector(".btn-start");
+const btnStop = document.querySelector(".btn-stop");
+const btnRestart = document.querySelector(".btn-restart");
 
 let emitter = new EventEmitter();
-emitter.subscribe('event:timerStart', data => {
-  timer.innerHTML = `timer is: ${data.time}`;
+//  ----------------------------------------------
+
+
+const state = {
+  'timerON': false,
+  'counter': 60
+}
+
+// add event listener to btnStart. If it clicks state btnStart becomes true
+// if it clicks another time state btnStart becomes false
+emitter.subscribe("event:timerON", (data) => {
+  state.timerON = data.timerON;
 });
 
+
+btnStart.addEventListener("click", () => {
+  emitter.emit("event:timerON", { timerON: state.timerON == true ? false : true });
+});
+
+
+function render(state) {
+
+  if (state.timerON && state.timerON >= 0) {
+    setInterval(() => {
+      state.counter = state.counter - 1;
+      console.log(state.counter);
+    }, 1000);
+
+  }
+
+  // if (state.timerON) {
+  //   let timer = state.counter;
+  //   let minutes, seconds;
+
+  //   setInterval(function test() {
+  //     minutes = parseInt(timer / 60, 10);
+  //     seconds = parseInt(timer % 60, 10);
+
+  //     minutes = minutes < 10 ? "0" + minutes : minutes;
+  //     seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  //     if (--timer < 0) {
+  //       timer = 0;
+  //     }
+  //   }, 1000);
+  // }
+
+
+  return `${state.counter}`
+}
+
+// По изменению состояния мы должны вызывать timerDir.innerHTML = render(state) // !!!
+
+// function render(state) {
+//   return `${state.counter}`
+// }
+
+// timerDir.innerHTML = render(state) // !!!
+// -----------------------------------------------
+
+
+// let timerValue = parseInt(timer.firstChild.nodeValue);
+
+
+
+// emitter.subscribe("event:timerStart", data => {
+//   timer.innerHTML = startTimer.test(data.timerValue);
+// });
+
+// btnStart.addEventListener("click", () => {
+//   emitter.emit("event:timerStart", { timerValue: timerValue });
+// });
+
+// function timerFn() {
+//   setInterval(timerStart(timerValue), 1000);
+// }
+
+// function timerStart(timerValue) {
+//   for (; timerValue >= 0; timerValue--) {
+//     setTimeout(console.log(timerValue), 1000);
+//   }
+// }
+
+// timerStart(timerValue);
+
+// -----------------------------------
+
+function startTimer(timerValue) {
+  let timer = timerValue;
+  let minutes, seconds;
+
+  setInterval(function test() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    if (--timer < 0) {
+      timer = 0;
+    }
+    return seconds;
+  }, 1000);
+}
