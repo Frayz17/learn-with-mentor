fc274fbe3d6f1cba825a763d960163b9544ac3f4
Andrey <andrydmit@gmail.com>
2019-02-15 10:17:58 +0200

    Standard style extension was embed and package.json added

--- timerPlainJS/main.js
+++ timerPlainJS/main.js
@@ -1,35 +1,35 @@
 
 class StatefulEmitter {
-  constructor(initialState) {
-    this.state = initialState,
+  constructor (initialState) {
+    this.state = initialState
     this.events = {}
   }
 
-  on(eventName, fn) {
+  on (eventName, fn) {
     if (!this.events[eventName]) {
-      this.events[eventName] = [];
+      this.events[eventName] = []
     }
 
-    this.events[eventName].push(fn);
+    this.events[eventName].push(fn)
 
     return () => {
-      this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+      this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn)
     }
   }
-  
-  emit(eventName, data) {
-    const handlers = this.events[eventName];
+
+  emit (eventName, data) {
+    const handlers = this.events[eventName]
     if (handlers.length) {
       for (let handler of handlers) {
-        handler.call(null, data);
+        handler.call(null, data)
       };
     }
   }
 
-  setState(newStateOrFn) {
+  setState (newStateOrFn) {
     if (typeof newStateOrFn === 'function') {
       this.state = newStateOrFn(this.state)
-     } else {
+    } else {
       this.state = newStateOrFn
     }
     this.emit(this.state)
@@ -37,75 +37,31 @@ class StatefulEmitter {
 }
 
 // data -----------------------------------------
-const timerDir = document.querySelector("#timer");
-const btnStart = document.querySelector(".btn-start");
-const btnStop = document.querySelector(".btn-stop");
-const btnRestart = document.querySelector(".btn-restart");
-let state$ = new StatefulEmitter();
+const timerDir = document.querySelector('#timer')
+const btnStart = document.querySelector('.btn-start')
+const btnStop = document.querySelector('.btn-stop')
+const btnRestart = document.querySelector('.btn-restart')
+let state$ = new StatefulEmitter()
 //  ---------------------------------------------
 // state$.on(state => timerDir.innerHTML = render(state))
 
-
-
 // add event listener to btnStart. If it clicks, state btnStart becomes - true
 // if it clicks another time, state btnStart becomes - false
-state$.on("", () => {
+state$.on('', () => {
 
-});
+})
 
-
-btnStart.addEventListener("click", () => {
-  state$.emit("", );
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btnStart.addEventListener('click', () => {
+  state$.emit('')
+})
 
 // state$.on("event:timerON", (data) => {
 //   state.timerON = data.timerON;
 // });
 
-
 // btnStart.addEventListener("click", () => {
 //   state$.emit("event:timerON", { timerON: state.timerON == true ? false : true });
 // });
-
-
-
-
-
-
 
 // function render(state) {
 //   if (state.timerON && state.timerON >= 0) {
@@ -127,7 +83,6 @@ btnStart.addEventListener("click", () => {
 //   //     minutes = minutes < 10 ? "0" + minutes : minutes;
 //   //     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        
 //   //     if (--timer < 0) {
 //   //       timer = 0;
 //   //     }
@@ -136,13 +91,3 @@ btnStart.addEventListener("click", () => {
 
 //   return `${state.counter}`
 // }
-
-
-
-
-
-
-
-
-
-
