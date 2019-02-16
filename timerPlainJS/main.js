e157a727a8da96c7343fa566c224faa775439761
Andrey <andrydmit@gmail.com>
2019-02-16 12:43:36 +0200

    emitter realization questions.

--- timerPlainJS/main.js
+++ timerPlainJS/main.js
@@ -1,5 +1,38 @@
 
 class StatefulEmitter {
+
+  // первоначально у меня был такой конструктор
+
+      // constructor (initialState) {
+      //   this.state = initialState
+      //   this.events = {}
+      // }
+
+  // this.state у меня принимала данные типа булеан и отвечала за состояние:
+  // включить/выключить таймер
+  // при такой реализации btnStart.addEventListener работает корректно и переключает состояние
+
+  // но когда я принялся реализовывать counter, то подумал, что свойство емиттера this.state
+  // у меня неправильно реализовано в принципе.  
+  // Оно не должно принимать данные типа булеан, а должно быть объектом, 
+  // который содержит все свойства состояния: "таймер включен" и "счетчик времени".
+  // Но с такой реализацией у меня метод setState перестал работать.
+  // Как модифицировать setState, что б он работал с this.state и this.events, в данной реализации ?
+
+
+
+  // В методе setState есть вызов метода "this.emit(this.state)"
+  // я не могу в полной мере разобраться как он работает в данном контексте, так как в первоначальной
+  // реализации, которую ты мне скинул, при срабатывании "btnStart.addEventListener" возвращалась ошибка
+  // Uncaught TypeError: Cannot read property 'length' of undefined
+
+  // Что бы избежать этой ошибки:
+  // В методе emit() я добавляю if (handlers !== undefined), тогда "btnStart.addEventListener" работает, 
+  // Мне кажется что это костыль, так как обработчик handler должен вызывать ивенты, в массиве events.
+  // но ведь и ивентов там получается никаких нет на данный момент.
+
+
+  
   constructor () {
     this.state = {
       timerON: false,
@@ -60,10 +93,9 @@ const btnStop = document.querySelector('.btn-stop')
 const btnRestart = document.querySelector('.btn-restart')
 let state$ = new StatefulEmitter()
 //  ---------------------------------------------
-// state$.on(state => timerDir.innerHTML = render(state))
 
 btnStart.addEventListener('click', () => {
-  let bool = state$.getState().timerON === true ? false : true
+  let bool = state$.getState().state === true ? false : true
   state$.setState(bool)
 
   if (btnStart.innerHTML === 'start') btnStart.innerHTML = 'pause'
@@ -74,6 +106,14 @@ btnStop.addEventListener('click', () => {
 
 })
 
+
+
+
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+// ------ Ниже черновики кода, можно не смотреть---------------
+// ------------------------------------------------------------
 
 
 
