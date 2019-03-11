76da0ef6cbeef4dde1bb0f8d7f461c2efb307af5
Andrey <andrydmit@gmail.com>
2019-03-11 14:07:38 +0200

    BMI calculator on react is done.

--- bmi-calculator/src/Components/BMI.js
+++ bmi-calculator/src/Components/BMI.js
@@ -10,10 +10,39 @@ class BMI extends Component {
     }
   }
 
+  handlerHeight = (event) => {
+    this.setState({
+      height: event.target.value
+    });
+  }
+
+  handlerWeight = (event) => {
+    this.setState({
+      weight: event.target.value
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h2>hello</h2>
+        <div>
+          <label htmlFor="height">height: </label>
+          <span>120 </span>
+          <input type="range" id="start" name="height" value={this.state.height} onChange={this.handlerHeight} min="120" max="230" />
+          <span>230</span>
+          <strong> {this.state.height} cm</strong>
+        </div>
+
+        <div>
+          <label htmlFor="weight">weight: </label>
+          <span>30 </span>
+          <input type="range" id="start" name="weight" value={this.state.weight} onChange={this.handlerWeight} min="30" max="280" />
+          <span>280</span>
+          <strong> {this.state.weight} kg</strong>
+        </div>
+
+        <br/>
+
         <CalcBMI height={this.state.height} weight={this.state.weight} />
       </React.Fragment>
     );
--- bmi-calculator/src/Components/CalcBMI.js
+++ bmi-calculator/src/Components/CalcBMI.js
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 const CalcBMI = (props) => {
   let BMIindex = (props.weight / (props.height ** 2) * 10000).toFixed(2);
 
+  function BMIstatus(BMIindex) {
+    return (
+      BMIindex <=18.5 ? "Undreweight"   :
+      BMIindex <=24.9 ? "Normal Weight" :
+      BMIindex <=29.9 ? "OverWeight"    :
+                        "Obese"
+    )
+  }
+
   return (
     <div>
-      {BMIindex}
+      BMI: <strong>{BMIindex} {" "} {BMIstatus(BMIindex)}</strong>
     </div>
   );
 }
 
-export default CalcBMI; 
+export default CalcBMI;
+
+
+// function tellBMI(bmi) {
+//   return bmi <= 18.5 ? "Underweight"   :
+//          bmi <= 24.9 ? "Normal Weight" :
+//          bmi <= 29.9 ? "Overweight"    :
+//                        "Obese"
+// }
