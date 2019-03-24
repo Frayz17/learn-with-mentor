0b750783405d6f57ae0e3cf9df7711a5eb3133d7
Andrey <andrydmit@gmail.com>
2019-03-24 12:26:37 +0200

    Create Async Await model in increment/decrement functions and setState.

--- counters/src/Components/Counter.js
+++ counters/src/Components/Counter.js
@@ -8,18 +8,29 @@ class Counter extends Component {
     }
   }
 
-  decrement = () => {
-    this.state.total > this.props.min &&
-    this.setState(state => ({
-      total: state.total - 1
-    }))
+
+  _setState(objOrFn) {
+    return new Promise(resolve => {
+      super.setState(objOrFn, resolve)
+    })
   }
 
-  increment = () => {
-    this.state.total < this.props.max &&
-    this.setState(prevState => ({
-      total: prevState.total + 1
-    }))
+  _increment = async () => {
+    if (this.state.total < this.props.max + 1) {
+      await this._setState({total: this.state.total + 1})
+      console.log('first call ', this.state) // +1
+      await this._setState({total: this.state.total + 1})
+      console.log('second call ', this.state) // +1
+    }
+  }
+
+  _decrement = async () => {
+    if (this.state.total > this.props.min + 1) {
+      await this._setState({total: this.state.total - 1})
+      console.log('first call ', this.state) // -1
+      await this._setState({total: this.state.total - 1})
+      console.log('second call ', this.state) // -1
+    }
   }
 
   reset = () => {
@@ -32,8 +43,8 @@ class Counter extends Component {
     return (
       <div>
         {this.props.title} {this.state.total} {" "}
-        <button id="btn-minus" onClick={this.decrement}>-1</button> {" "}
-        <button id="btn-plus" onClick={this.increment}>+1</button> {" "}
+        <button id="btn-minus" onClick={this._decrement}>-1</button> {" "}
+        <button id="btn-plus" onClick={this._increment}>+1</button> {" "}
         <button id="btn-reset" onClick={this.reset}>Reset</button> {" "}
       </div>
     );
